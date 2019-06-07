bb1af1c9703a0d293759ae49e87514e548de8705
Hougo <hougo13@gmail.com>
2019-06-07 13:30:38 +0200

    seo

--- config/site.js
+++ config/site.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
-  siteLanguage: 'en', // Language Tag on <html> element
+  title: 'BDE CESI AIX', // Navigation and Site Title
+  titleAlt: 'BDE CESI AIX', // Title for JSONLD
+  description:
+    "Le site du bureau des étudiants du CESI d'Aix-en-Provence (BDE CESI AIX)",
+  url: 'https://bde-cesi-aix.fr', // Domain of your site. No trailing slash!
+  siteUrl: 'https://bde-cesi-aix.fr', // url + pathPrefix
+  siteLanguage: 'fr', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  shortName: 'BDECESIAIX', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'BDE CESI AIX', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
 };
