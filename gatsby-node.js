3a42f6e47a984ef8c70301eaabd033d66c089489
Hougo <hougo13@gmail.com>
2019-10-06 21:31:47 +0200

    update relative path

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,18 @@
 const path = require('path');
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  fmImagesToRelative(node);
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode });
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    });
+  }
+};
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
