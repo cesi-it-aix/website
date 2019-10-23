a32037d411b4d34090fe8a58602df5d667362cd7
BlackWolfie <luc.vandevelde@viacesi.com>
2019-10-23 11:45:51 +0200

    Co-authored-by: RENARD Hugo <hugo.renard@soprasteria.com>
    add gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,33 +1,36 @@
-import React, { useState, useCallback } from "react";
-import { render } from "react-dom";
-import {graphql, useStaticQuery} from "gatsby"
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import { photos } from "../Image/photos";
+import React, { useState, useCallback } from 'react';
+import { render } from 'react-dom';
+import { graphql, useStaticQuery } from 'gatsby';
+import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import { photos } from '../Image/photos';
 
-
-const getQuery = folder => `
- {
-    allFile(filter: {relativeDirectory: {eq: ${folder}}}) {
-        edges {
-            node {
-                childImageSharp {
-                    sizes(maxHeight: 10, maxWidth: 10) {
-                        src
-                        presentationWidth
-                        presentationHeight
-                    }
-                }
+const GET_PICTURES = graphql`
+  {
+    allFile(filter: { absolutePath: { regex: "/Gallery/" } }) {
+      edges {
+        node {
+          absolutePath
+          childImageSharp {
+            original {
+              src
+              height
+              width
             }
+          }
         }
+      }
     }
-}
-`
+  }
+`;
 
-const ImageGall = ({folder}) => {
-    const pictures = useStaticQuery(graphql(getQuery(folder)));
-    return <Gallery photos={pictures} />
+const ImageGall = ({ folder }) => {
+  const data = useStaticQuery(GET_PICTURES);
+  const pictures = data.allFile.edges
+    .filter(x => x.node.absolutePath.includes(`Gallery/${folder}`))
+    .map(x => x.node.childImageSharp.original);
+
+  return <Gallery photos={pictures} />;
 };
 
 export default ImageGall;
-
